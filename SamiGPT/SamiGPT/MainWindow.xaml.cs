5018fae7df39099e3180b4c1fc504398195e47da
User <samibenmaammargb1@gmail.com>
2023-08-27 17:58:37 +0200

    init project and sqeleton

---
+++ SamiGPT/SamiGPT/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Windows;
+
+
+namespace SamiGPT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dragWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void minMax_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
+        }
+
+        private void minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
